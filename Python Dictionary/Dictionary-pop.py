3cba77bdbb84f58ddf79dfabc71a28b9685df79a
kbhat1234 <karthikbhat@live.com>
2020-04-19 22:59:52 +0530

    modified some changes

--- Python Dictionary/Dictionary-pop.py
+++ Python Dictionary/Dictionary-pop.py
@@ -4,6 +4,8 @@ print(f'dic is {dic}')
 Output:
 dic is {1: 'apple', 2: 'orange', 3: 'grapes', 4: 'mango', 5: 'banana'}
 '''
+
+# valid key present in dictionary
 result = dic.pop(5)
 print(f'popped element is {result}')
 print(f'dic after popped out element is {dic}')
@@ -13,6 +15,7 @@ popped element is banana
 dic after popped out element is {1: 'apple', 2: 'orange', 3: 'grapes', 4: 'mango'}
 '''
 
+# invalid key/key not present in dictionary, but default message is provided
 res = dic.pop(6, 'no element found in dic')
 print(f'popped out element is {res}')
 print(f'dic after pop is {dic}')
@@ -21,6 +24,8 @@ Output:
 popped out element is no element found in dic
 dic after pop is {1: 'apple', 2: 'orange', 3: 'grapes', 4: 'mango'}
 '''
+
+# key not present in dictionary and no default message provided
 #res1 = dic.pop(7)  # KeyError shown
 #print(res1)
 '''
