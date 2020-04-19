d0bfd2e97e3270691118147adbe032626d0c3322
kbhat1234 <karthikbhat@live.com>
2020-04-19 22:45:03 +0530

    add

---
+++ Python Dictionary/Dictionary-pop.py
@@ -0,0 +1,32 @@
+dic = {1: 'apple', 2: 'orange', 3: 'grapes', 4: 'mango', 5: 'banana'}
+print(f'dic is {dic}')
+'''
+Output:
+dic is {1: 'apple', 2: 'orange', 3: 'grapes', 4: 'mango', 5: 'banana'}
+'''
+result = dic.pop(5)
+print(f'popped element is {result}')
+print(f'dic after popped out element is {dic}')
+'''
+Output:
+popped element is banana
+dic after popped out element is {1: 'apple', 2: 'orange', 3: 'grapes', 4: 'mango'}
+'''
+
+res = dic.pop(6, 'no element found in dic')
+print(f'popped out element is {res}')
+print(f'dic after pop is {dic}')
+'''
+Output:
+popped out element is no element found in dic
+dic after pop is {1: 'apple', 2: 'orange', 3: 'grapes', 4: 'mango'}
+'''
+#res1 = dic.pop(7)  # KeyError shown
+#print(res1)
+'''
+Output:
+Traceback (most recent call last):
+  File "C:/Users/karth/PycharmProjects/PythonPackage/Programs/Python Dictionary/Dictionary-pop.py", line 12, in <module>
+    res1 = dic.pop(7)
+KeyError: 7
+'''
