d298cee644ed35bd438f601c5d345b39ca3ea3de
kbhat1234 <karthikbhat@live.com>
2020-04-26 00:24:43 +0530

    adding diff function

--- Functions/Function2.py
+++ Functions/Function2.py
@@ -1,7 +1,13 @@
 def Sum(x, y):
     return x + y
 
+
+def diff(x, y):
+    return x - y
+
+
 a = int(input('enter a '))
 b = int(input('enter b '))
 
 print(f'sum is {Sum(a, b)}')
+print(f'diff is {diff(a, b)}')
