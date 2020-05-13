ca3e4399113d83417d057e5c276649ffc9f2e8ff
kbhat1234 <karthikbhat@live.com>
2020-05-13 19:43:37 +0530

    add json codes

---
+++ JSON/JSONMethod-dump.py
@@ -0,0 +1,87 @@
+import json as j
+
+
+def dic_display():
+    print(type(student))
+    print(student)
+    print(student['firstname'])
+    print(student['rollno'])
+    print(student['subject'][-1])
+    print(student['subject'])
+    print(student['gender'])
+    print(student['gender']['man'])
+    print(student['gender']['woman'])
+
+
+def tup_display():
+    print(f'student1 tuple is {student1}')
+    print(type(student1))
+    print(student1[2:])
+    print(student1[-3])
+
+
+def li_display():
+    print(f'student2 list is {student2}')
+    print(type(student2))
+    print(student2[2])
+
+
+# student dictionary
+student = {
+    'firstname': 'Karthik',
+    'lastname': 'Bhat',
+    'rollno': 534535,
+    'grade': 'A',
+    'subject': ['english', 'maths', 'computers'],
+    'gender': {'man': 'male', 'woman': 'female'}
+}
+
+# open or create a file with write mode to student.json
+with open('student.json', 'w') as file_write:
+    # using dump() function, convert python data object (dictionary) to JSON data object and write to file
+    j.dump(student, file_write)
+    file_write.close()
+dic_display()
+'''
+Output:
+<class 'dict'>
+{'firstname': 'Karthik', 'lastname': 'Bhat', 'rollno': 534535, 'grade': 'A', 'subject': ['english', 'maths', 'computers'], 'gender': {'man': 'male', 'woman': 'female'}}
+Karthik
+534535
+computers
+['english', 'maths', 'computers']
+{'man': 'male', 'woman': 'female'}
+male
+female
+'''
+
+# student1 tuple
+student1 = ('karthik', 20, 20033.88, 'male')
+
+# open or create file with open mode to write to student1.json
+with open('student1.json', 'w') as file_write:
+    j.dump(student1, file_write)
+    file_write.close()
+tup_display()
+'''
+Output:
+student1 tuple is ('karthik', 20, 20033.88, 'male')
+<class 'tuple'>
+(20033.88, 'male')
+20
+'''
+
+# student2 list
+student2 = ['karthik', 20, 20033.88, 'male']
+
+# open or create file with write mode to student2.json
+with open('student2.json', 'w') as file_write:
+    j.dump(student2, file_write)
+    file_write.close()
+li_display()
+'''
+Output:
+student2 list is ['karthik', 20, 20033.88, 'male']
+<class 'list'>
+20033.88
+'''
---
+++ JSON/JSONMethod-dumps.py
@@ -0,0 +1,99 @@
+import json as j
+
+
+# functions definition defined here
+def dic_display():
+    print(student['fname'])
+    print(student['salary'])
+    print(student['gender']['man'])
+    print(student['contact no'])
+    print(student['landline'])
+    print(type(student))
+    print(type(d1))
+    print(f'student dic is {student}')
+
+
+def tup_display():
+    print(student1[0])
+    print(student1[:2])
+    print(student1[-1])
+    print(f'student1 tup is {student1}')
+    print(type(student1))
+    print(type(d2))
+
+
+def li_display():
+    print(f'student2 list is {student2}')
+    print(type(student2))
+    print(type(d3))
+    print(student2[0:2])
+    print(student2[-3])
+
+
+# student dictionary
+student = {
+    'fname': 'Karthik',
+    'lname': 'Bhat',
+    'mname': 'K',
+    'age': 40,
+    'salary': 2500000.77,
+    'gender': {'man': 'male', 'woman': 'female'},
+    'contact no': True,
+    'mobile': 9886867677,
+    'landline': None
+}
+
+# student1 tuple
+student1 = ('karthik', 23233, 40, 3423434.232)
+
+# student2 list
+student2 = ['ishani', 'kaustubh', 'rini', 'karthik']
+
+# convert python dictionary object to JSON string
+d1 = j.dumps(student)
+print(f'convert python object (dictionary) to JSON string is \n{d1}')
+dic_display()
+'''
+Output:
+convert python object (dictionary) to JSON string is 
+{"fname": "Karthik", "lname": "Bhat", "mname": "K", "age": 40, "salary": 2500000.77, "gender": {"man": "male", "woman": "female"}, "contact no": true, "mobile": 9886867677, "landline": null}
+Karthik
+2500000.77
+male
+True
+None
+<class 'dict'>
+<class 'str'>
+student dic is {'fname': 'Karthik', 'lname': 'Bhat', 'mname': 'K', 'age': 40, 'salary': 2500000.77, 'gender': {'man': 'male', 'woman': 'female'}, 'contact no': True, 'mobile': 9886867677, 'landline': None}
+'''
+
+# convert python tuple object to JSON string
+d2 = j.dumps(student1)
+print(f'convert python object (tuple) to JSON string is \n{d2}')
+tup_display()
+'''
+Output:
+convert python object (tuple) to JSON string is 
+["karthik", 23233, 40, 3423434.232]
+karthik
+('karthik', 23233)
+3423434.232
+student1 tup is ('karthik', 23233, 40, 3423434.232)
+<class 'tuple'>
+<class 'str'>
+'''
+
+# convert python list object to JSON string
+d3 = j.dumps(student2)
+print(f'convert python object (list) to JSON string is \n{d3}')
+li_display()
+'''
+Output:
+convert python object (list) to JSON string is 
+["ishani", "kaustubh", "rini", "karthik"]
+student2 list is ['ishani', 'kaustubh', 'rini', 'karthik']
+<class 'list'>
+<class 'str'>
+['ishani', 'kaustubh']
+kaustubh
+'''
