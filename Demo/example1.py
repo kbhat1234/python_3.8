f7f7541d993796dee31f1337763aada5bb10e46a
kbhat1234 <karthikbhat@live.com>
2020-12-24 17:10:58 +0530

    add

--- Demo/example1.py
+++ Demo/example1.py
@@ -27,17 +27,6 @@ print("Full concatenated string is ", str1+str2)
 print("Hello, ", str1)
 print("Hello, ", str2)
 
-# isinstance() checks if the object (first argument) is an instance or subclass of classinfo class (second argument).
-
-print(isinstance(str1, str))
-print(isinstance(f, int))
-print(isinstance(c, float))
-print(isinstance(str2, (int, float, str)))
-print(isinstance(str(f), (int, float, str)))
-print(isinstance(str(f), int))
-print(isinstance(str(f), float))
-print(isinstance(str(f), str))
-
 print(str1*2)
 print(str1[0:4])
 print(str1[4:])
@@ -45,9 +34,7 @@ print(str1[0:])
 print(str1[:])
 
 
-def StringOperations():
-    str3 = "Welcome to "
-    str4 = "Python "
+def StringOperations1():
     print("Full concatenated string is ", str1 + str2)
     print(str1*4)
     print((str1*4)*2)
@@ -55,5 +42,37 @@ def StringOperations():
     print("Hello, ", str2)
 
 
-StringOperations()
+def StringOperations2():
+    print(str1 * 2)
+    print(str1[0:4])
+    print(str1[4:])
+    print(str1[0:])
+    print(str1[:])
+    StringOperations1()
 
+
+def IsInstance():
+    # isinstance() checks if the object (first argument) is an instance or subclass of classinfo class (second argument).
+    print(isinstance(str1, str))
+    print(isinstance(f, int))
+    print(isinstance(c, float))
+    print(isinstance(str2, (int, float, str)))
+    print(isinstance(str(f), (int, float, str)))
+    print(isinstance(str(f), int))
+    print(isinstance(str(f), float))
+    print(isinstance(str(f), str))
+    StringOperations1()
+    StringOperations2()
+
+# StringOperations1()
+# StringOperations2()
+# IsInstance()
+
+s1 = "Karthik"
+s2 = "Rini Das Bhat"
+
+print(s1 in s2)
+print(s1 not in s2)
+
+print("Bhat" in s1)
+print("Bhat" in s2)
