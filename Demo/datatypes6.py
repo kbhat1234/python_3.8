3e0c2da7512e142237a8b3ca7aba2652c8a9182e
kbhat1234 <karthikbhat@live.com>
2020-12-29 14:06:55 +0530

    add

---
+++ Demo/datatypes6.py
@@ -0,0 +1,17 @@
+set1 = set()
+print(set1)
+
+set1.add(10)
+set1.add(10)
+set1.add(20)
+
+print(set1)
+set1.add(30)
+set1.add(40)
+
+print(set1)
+set1.pop()
+print(set1)
+
+set1.clear()
+print(set1)
